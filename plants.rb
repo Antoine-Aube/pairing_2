1a294fb958523e468b1b4a18ddddc72cd1c94032
Antoine Aube <aaube3@gmail.com>
2023-06-01 15:39:48 -0600

    Add or conditional

--- plants.rb
+++ plants.rb
@@ -1,8 +1,10 @@
-gets_sunshine = true
+gets_sunshine = false
 ounce_of_water = 10
 
 if gets_sunshine == true && ounce_of_water >= 10
     puts "Thrivin'"
+elsif gets_sunshine == true || ounce_of_water >= 10
+    puts "Thrivin' but not jivin'"
 else
     puts "Dead"
 end 
